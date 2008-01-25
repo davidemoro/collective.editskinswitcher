3aac179cae5b8678b25783b297c20b589d4b5463
Maurits van Rees <maurits@vanrees.org>
2008-01-25 19:32:14 +0000

    Rename README.txt to tests/setup.txt as it is too technical. A more readable version is coming up.
    svn path=/collective.editskinswitcher/trunk/; revision=57736

--- collective.editskinswitcher/collective/editskinswitcher/tests/test_setup.py
+++ collective.editskinswitcher/collective/editskinswitcher/tests/test_setup.py
@@ -1,6 +1,4 @@
 import unittest
-#from zope.testing import doctestunit
-#from zope.component import testing
 from Testing import ZopeTestCase as ztc
 from base import BaseTestCase
 
@@ -8,24 +6,10 @@ from base import BaseTestCase
 def test_suite():
     return unittest.TestSuite([
 
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='collective.editskinswitcher',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='collective.editskinswitcher.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
         # Integration tests that use PloneTestCase
         ztc.ZopeDocFileSuite(
-            'README.txt', package='collective.editskinswitcher',
+            'setup.txt', package='collective.editskinswitcher.tests',
             test_class=BaseTestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='collective.editskinswitcher',
-        #    test_class=BaseTestCase),
         
         ])
 
