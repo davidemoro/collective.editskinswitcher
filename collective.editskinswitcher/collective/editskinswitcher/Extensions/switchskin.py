7e10bbc538e78ec551f29182161e9c2760e8ac38
Maurits van Rees <maurits@vanrees.org>
2008-01-28 12:47:05 +0000

    Check for the based_on_url property. Test that no errors are throws when our property sheet is gone.
    svn path=/collective.editskinswitcher/trunk/; revision=57812

--- collective.editskinswitcher/collective/editskinswitcher/Extensions/switchskin.py
+++ collective.editskinswitcher/collective/editskinswitcher/Extensions/switchskin.py
@@ -5,16 +5,21 @@ from Products.CMFCore.utils import getToolByName
 def switch_skin(context, REQUEST=None):
     """Switch to the Plone Default skin when we are editing.
     """
-    url = REQUEST.getURL()
-    if is_edit_url(url):
-        edit_skin = ''
-        portal_props = getToolByName(context, 'portal_properties')
-        if portal_props is not None:
-            editskin_props = portal_props.get('editskin_switcher')
-            if editskin_props is not None:
-                edit_skin = editskin_props.getProperty('edit_skin', '')
-        # If edit_skin is an empty string or the skin does not exist,
-        # the next call is intelligent enough to use the default skin
-        # instead.
-        context.changeSkin(edit_skin, REQUEST)
+    portal_props = getToolByName(context, 'portal_properties')
+    if portal_props is None:
+        return None
+    editskin_props = portal_props.get('editskin_switcher')
+    if editskin_props is None:
+        return None
+    # Okay, we have a property sheet we can use.
+    edit_skin = editskin_props.getProperty('edit_skin', '')
+    based_on_url = editskin_props.getProperty('based_on_url', True)
+    need_authentication = editskin_props.getProperty('need_authentication',
+                                                     False)
+    if based_on_url:
+        url = REQUEST.getURL()
+        if is_edit_url(url):
+            # If the edit_skin does not exist, the next call is
+            # intelligent enough to use the default skin instead.
+            context.changeSkin(edit_skin, REQUEST)
     return None
