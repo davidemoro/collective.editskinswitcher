7d0f55569e2ef940025bf8beba684e05e0fbaf72
Maurits van Rees <maurits@vanrees.org>
2010-11-26 22:20:19 +0000

    Use less test files, in an attempt to clear up some Plone 3 test failures which seem to have to do with test setup.
    svn path=/collective.editskinswitcher/branches/per-folder-default-skin/; revision=227647

--- collective/editskinswitcher/tests/test_url_switch.py
+++ collective/editskinswitcher/tests/test_url_switch.py
@@ -6,7 +6,8 @@ from base import BaseTestCase
 def test_suite():
     return unittest.TestSuite([
 
-        # Integration tests that use PloneTestCase
+        # Integration tests that use PloneTestCase but no functional
+        # test browser.
         ztc.ZopeDocFileSuite(
             'url_switch.txt', package='collective.editskinswitcher.tests',
             test_class=BaseTestCase),
