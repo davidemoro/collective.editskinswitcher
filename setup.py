76352d9603bf00be0f4233344c9f6b8e5e9bbba1
Maurits van Rees <maurits@vanrees.org>
2010-12-28 17:11:33 +0000

    Rename docs/HISTORY.txt to CHANGES.txt
    svn path=/collective.editskinswitcher/trunk/; revision=229538

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readmefile = open(os.path.join('collective', 'editskinswitcher', 'README.txt'))
 readme = readmefile.read().strip()
 readmefile.close()
 
-historyfile = open(os.path.join('docs', 'HISTORY.txt'))
+historyfile = open('CHANGES.txt')
 history = historyfile.read().strip()
 historyfile.close()
 
