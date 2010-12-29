a750623da645bd595cd446eb4ab0f31d25b32e72
Maurits van Rees <maurits@vanrees.org>
2010-12-29 14:25:37 +0000

    Moved some logic from browser/view.py into skin.py: methods add_hook and remove_hook. This makes it easier to reuse only that code without any extra portal status messages or redirects.
    svn path=/collective.editskinswitcher/trunk/; revision=229581

--- collective/editskinswitcher/browser/view.py
+++ collective/editskinswitcher/browser/view.py
@@ -1,12 +1,8 @@
 import logging
 from zope.interface import alsoProvides, noLongerProvides
-from Acquisition import aq_base, aq_inner
+from Acquisition import aq_inner
 from Products.CMFPlone.utils import getToolByName
 from plone.app.layout.navigation.interfaces import INavigationRoot
-from Products.Five.component import LocalSiteHook, HOOK_NAME
-from Products.SiteAccess.AccessRule import AccessRule
-from ZPublisher.BeforeTraverse import registerBeforeTraverse
-from ZPublisher.BeforeTraverse import unregisterBeforeTraverse
 from zope.component import getUtility
 from zope.publisher.browser import BrowserView
 
@@ -19,9 +15,9 @@ except ImportError:
     from zope.app.publisher.interfaces.browser import IBrowserMenu
 
 from collective.editskinswitcher import SwitcherMessageFactory as _
-from collective.editskinswitcher.skin import get_selected_default_skin
-from collective.editskinswitcher.skin import set_selected_default_skin
-from collective.editskinswitcher import config
+from collective.editskinswitcher.skin import (
+    get_selected_default_skin, set_selected_default_skin,
+    add_hook, remove_hook)
 
 logger = logging.getLogger('collective.editskinswitcher')
 
@@ -31,7 +27,6 @@ class SelectSkin(BrowserView):
     def update(self):
         """Set selected skin as the default for the current folder."""
         context = aq_inner(self.context)
-        base_object = aq_base(context)
         utils = getToolByName(context, "plone_utils")
 
         # Which skin is requested?
@@ -53,36 +48,11 @@ class SelectSkin(BrowserView):
         elif skin_name is None and current_skin is not None:
             # Need to remove the hook.
             utils.addPortalMessage(_(u"No default skin selected anymore."))
-            if hasattr(base_object, HOOK_NAME):
-                logger.info("Unregistering before traverse hook at %s",
-                            context.absolute_url())
-                unregisterBeforeTraverse(base_object, HOOK_NAME)
-                # Now we should be able to remove the local site hook.
-                # But this might not always be the best idea.  Others
-                # might be using the local site hook as well.  Let's
-                # at least make it configurable:
-                if config.REMOVE_LOCAL_SITE_HOOK:
-                    logger.info("Removing local site hook at %s",
-                                context.absolute_url())
-                    delattr(base_object, HOOK_NAME)
-                else:
-                    logger.info("Keeping local site hook at %s",
-                                context.absolute_url())
+            remove_hook(context)
         else:
             # The normal case: a change to the default skin.
             utils.addPortalMessage(_(u"Skin changed."))
-
-            # Check to see if the current object already has a
-            # LocalSiteHook from Five registered. If not, then register
-            # one ourselves, going around ``enableSite`` since we don't
-            # want to make this object a full ``ISite``, but just get the
-            # ``BeforeTraverseEvent`` fired.
-            if not hasattr(base_object, HOOK_NAME):
-                logger.info("Adding local site hook with before traverse hook "
-                            "at %s", context.absolute_url())
-                hook = AccessRule(HOOK_NAME)
-                registerBeforeTraverse(base_object, hook, HOOK_NAME, 1)
-                setattr(base_object, HOOK_NAME, LocalSiteHook())
+            add_hook(context)
 
         # Finally set the default skin.  Note that this is safe to
         # call when skin_name is None as well, as this cleans up a
--- collective/editskinswitcher/skin.py
+++ collective/editskinswitcher/skin.py
@@ -1,8 +1,14 @@
 import logging
+from Acquisition import aq_base
+from Acquisition import aq_inner
 from persistent.mapping import PersistentMapping
 from zope.annotation.interfaces import IAnnotations
+from Products.Five.component import LocalSiteHook, HOOK_NAME
+from Products.SiteAccess.AccessRule import AccessRule
+from ZPublisher.BeforeTraverse import registerBeforeTraverse
+from ZPublisher.BeforeTraverse import unregisterBeforeTraverse
 
-from Acquisition import aq_inner
+from collective.editskinswitcher import config
 
 logger = logging.getLogger('collective.editskinswitcher')
 ANNOTATION_KEY = "collective.editskinswitcher"
@@ -40,3 +46,40 @@ def set_selected_default_skin(context, skin_name=None):
         logger.info("Set the default skin of %s to %s.",
                     context.absolute_url(), skin_name)
         ns["default-skin"] = skin_name
+
+
+def add_hook(context):
+    """Add local site hook with before traverse event.
+
+    Check to see if the current object already has a LocalSiteHook
+    from Five registered. If not, then register one ourselves, going
+    around ``enableSite`` since we don't want to make this object a
+    full ``ISite``, but just get the ``BeforeTraverseEvent`` fired.
+    """
+    base_object = aq_base(context)
+    if hasattr(base_object, HOOK_NAME):
+        return
+    logger.info("Adding local site hook with before traverse hook at %s",
+                context.absolute_url())
+    hook = AccessRule(HOOK_NAME)
+    registerBeforeTraverse(base_object, hook, HOOK_NAME, 1)
+    setattr(base_object, HOOK_NAME, LocalSiteHook())
+
+
+def remove_hook(context):
+    """Remove local site hook with before traverse event.
+    """
+    base_object = aq_base(context)
+    if not hasattr(base_object, HOOK_NAME):
+        return
+    logger.info("Unregistering before traverse hook at %s",
+                context.absolute_url())
+    unregisterBeforeTraverse(base_object, HOOK_NAME)
+    # Now we should be able to remove the local site hook.  But this
+    # might not always be the best idea.  Others might be using the
+    # local site hook as well.  Let's at least make it configurable:
+    if config.REMOVE_LOCAL_SITE_HOOK:
+        logger.info("Removing local site hook at %s", context.absolute_url())
+        delattr(base_object, HOOK_NAME)
+    else:
+        logger.info("Keeping local site hook at %s", context.absolute_url())
