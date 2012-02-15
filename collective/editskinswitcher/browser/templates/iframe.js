9568c5154ca13a930183f3a1b160f14fa92261bc
Mirella van Teulingen <spacebabe77@gmail.com>
2012-02-15 14:21:31 +0000

    changed the calculation for the iframe height. With the original code Firefox was behaving quircky, the height was 150px. That is too small. With help of Mark we could fix this.
    svn path=/collective.editskinswitcher/trunk/; revision=248421

--- collective/editskinswitcher/browser/templates/iframe.js
+++ collective/editskinswitcher/browser/templates/iframe.js
@@ -1,7 +1,13 @@
 // stretch the preview iframe to fit the edit interface
 
 function calcHeight()
-{
-  iframe = document.getElementById('editskinswitcher-preview');
-  iframe.height=iframe.contentWindow.document.body.scrollHeight;
+{ 
+  var iframe = document.getElementById('editskinswitcher-preview');
+
+  if (iframe) {
+        var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+        if (iframeWin.document.body) {
+            iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+        }
+    }
 }
