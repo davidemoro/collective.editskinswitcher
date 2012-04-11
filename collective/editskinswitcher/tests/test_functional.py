d4e2703d0b38235522c6640ab2422f66657bbb7b
Maurits van Rees <maurits@vanrees.org>
2012-04-11 15:22:30 +0000

    Introduce a gazillion test layers and classes so the browser tests are more isolated.
    svn path=/collective.editskinswitcher/trunk/; revision=249325

--- collective/editskinswitcher/tests/test_functional.py
+++ collective/editskinswitcher/tests/test_functional.py
@@ -1,6 +1,53 @@
 import unittest
 from Testing import ZopeTestCase as ztc
 from collective.editskinswitcher.tests import base
+from collective.editskinswitcher.tests.layer import TestLayer
+
+
+# There is too much funkyness going on with tests that pass just fine
+# when run on their own but fail when combined with the other tests.
+# This is probably because we have to do funky stuff in order to test
+# changing skins.  So we introduce more test layers so we have more
+# isolation.  Funky indeed...
+
+class TestLayer1(TestLayer):
+    pass
+
+
+class FTC1(base.BaseFunctionalTestCase):
+    layer = TestLayer1
+
+
+class TestLayer2(TestLayer):
+    pass
+
+
+class FTC2(base.BaseFunctionalTestCase):
+    layer = TestLayer2
+
+
+class TestLayer3(TestLayer):
+    pass
+
+
+class FTC3(base.BaseFunctionalTestCase):
+    layer = TestLayer3
+
+
+class TestLayer4(TestLayer):
+    pass
+
+
+class FTC4(base.BaseFunctionalTestCase):
+    layer = TestLayer4
+
+
+class TestLayer5(TestLayer):
+    pass
+
+
+class FTC5(base.BaseFunctionalTestCase):
+    layer = TestLayer5
 
 
 def test_suite():
@@ -11,22 +58,22 @@ def test_suite():
         ztc.ZopeDocFileSuite(
             'tests/force_login.txt',
             package='collective.editskinswitcher',
-            test_class=base.BaseFunctionalTestCase),
+            test_class=FTC1),
 
         ztc.ZopeDocFileSuite(
             'tests/ssl_switch.txt',
             package='collective.editskinswitcher',
-            test_class=base.BaseFunctionalTestCase),
+            test_class=FTC2),
 
         ztc.ZopeDocFileSuite(
             'tests/need_authentication.txt',
             package='collective.editskinswitcher',
-            test_class=base.BaseFunctionalTestCase),
+            test_class=FTC3),
 
         ztc.ZopeDocFileSuite(
             'tests/ploneadmin_header.txt',
             package='collective.editskinswitcher',
-            test_class=base.BaseFunctionalTestCase),
+            test_class=FTC4),
 
         ztc.ZopeDocFileSuite(
             'tests/preview.txt',
@@ -36,6 +83,6 @@ def test_suite():
         ztc.ZopeDocFileSuite(
             'tests/specific_switch.txt',
             package='collective.editskinswitcher',
-            test_class=base.BaseFunctionalTestCase),
+            test_class=FTC5),
 
         ])
