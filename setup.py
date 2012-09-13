9641c4f1407a37a68217c1241a201687c55735cc
Maurits van Rees <maurits@vanrees.org>
2012-09-13 15:32:02 +0200

    Move version to setup.py.

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
 from setuptools import setup, find_packages
-import os
-
-versionfile = open(os.path.join('collective', 'editskinswitcher', 'version.txt'))
-version = versionfile.read().strip()
-versionfile.close()
 
 readme = open('README.txt').read().strip()
-
 history = open('CHANGES.rst').read().strip()
-
 long_description = "%s\n\n\n%s" % (readme, history)
 
 setup(name='collective.editskinswitcher',
-      version=version,
+      version='2.4.dev0',
       description="Switch to the edit skin for certain domains.""",
       long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Zope2",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Zope2",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='',
       author='Maurits van Rees',
       author_email='m.van.rees@zestsoftware.nl',
