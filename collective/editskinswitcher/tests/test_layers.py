f8c38e3fd8b7948a9cc3bad865ea77cff1fa3a54
Davide Moro <davide.moro@redomino.com>
2013-09-16 09:41:22 +0200

    found a bug?

--- collective/editskinswitcher/tests/test_layers.py
+++ collective/editskinswitcher/tests/test_layers.py
@@ -153,6 +153,57 @@ class TestLayers(base.LayersTestCase):
         self.assertTrue(ILayerTest2 in portal.REQUEST.__provides__.__iro__)
         self.assertEquals('layer2\n', self.portal.restrictedTraverse('@@layer_view')())
 
+    def test_layers_specific_domains_3(self):
+        """ Test with the specific_domains option with http://edit.bar.com/something"""
+        portal = self.portal
+        from collective.editskinswitcher.tests.interfaces import ILayerTest1
+        from collective.editskinswitcher.tests.interfaces import ILayerTest2
+        # the request should be marked with the ILayerTest1 but Plone testing
+        # tool kits won't register layers for you.
+        # See http://developer.plone.org/views/layers.html#testing-layers
+        self.assertFalse(ILayerTest1 in portal.REQUEST.__provides__.__iro__)
+        self.assertFalse(ILayerTest2 in portal.REQUEST.__provides__.__iro__)
+
+        # Default theme: Layertest 1
+        self.assertEquals('Layertest 1', portal.portal_skins.getDefaultSkin())
+
+        # Fallback edit theme: Layertest 2
+        from Products.CMFCore.utils import getToolByName
+        portal_props = getToolByName(portal, 'portal_properties')
+        editskin_props = portal_props.get('editskin_switcher')
+        editskin_props.edit_skin = 'Layertest 2'
+        editskin_props.specific_domains = 'http://edit.bar.com'
+        editskin_props.switch_skin_action = 'based on specific domains'
+
+        from collective.editskinswitcher.tests.utils import TestRequest
+        from collective.editskinswitcher.tests.utils import FakeTraversalEvent
+        from collective.editskinswitcher.traversal import switch_skin
+        request = TestRequest(SERVER_URL='http://localhost')
+        portal.REQUEST = request
+        event = FakeTraversalEvent(portal, request)
+        switch_skin(portal, event)
+        self.assertEquals('Layertest 1', portal.getCurrentSkinName())
+
+        # no change skin, so add the ILayerTest1 manually for now since
+        # Plone testing tool kits won't register layers for you
+        from zope.interface import directlyProvides
+        directlyProvides(portal.REQUEST, ILayerTest1)
+        # After the switch_skin the ILayerTest1 should be active
+        self.assertTrue(ILayerTest1 in portal.REQUEST.__provides__.__iro__)
+        self.assertFalse(ILayerTest2 in portal.REQUEST.__provides__.__iro__)
+        self.assertEquals('layer1\n', self.portal.restrictedTraverse('@@layer_view')())
+
+        request = TestRequest(SERVER_URL='http://edit.bar.com/something')
+        portal.REQUEST = request
+        event = FakeTraversalEvent(portal, request)
+        switch_skin(portal, event)
+        self.assertEquals('Layertest 2', portal.getCurrentSkinName())
+
+        # After the switch_skin the ILayerTest2 should be active
+        self.assertFalse(ILayerTest1 in portal.REQUEST.__provides__.__iro__)
+        self.assertTrue(ILayerTest2 in portal.REQUEST.__provides__.__iro__)
+        self.assertEquals('layer2\n', self.portal.restrictedTraverse('@@layer_view')())
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
